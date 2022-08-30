c690edff032d74687fa56da1c0b9cab5d0ce8725
MatveyKuba <110122738+MatveyKuba@users.noreply.github.com>
2022-08-30 07:43:15 +0500

    Загрузил для вопроса по ДЗ 11-12

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -79,6 +79,24 @@ $("select").on("change", function(){
     $("#price .number").text(days)
 })
 
+         $(window).scroll(() => {
+               let scrollDistance = $(window).scrollTop();
+
+               $('section').each((i, el) => {
+
+                   if ($(el).offset().top - $('nav').outerHeight() <= scrollDistance) {
+                       $('nav a').each((i, el) => {
+                           if ($(el).hasClass('active')) {
+                               $(el).removeClass('active');
+                           }
+                       });
+
+                       $('nav li:eq(' + i + ')').find('a').addClass('active');
+                   }
+
+               });
+           });
+    
 })
 
  
